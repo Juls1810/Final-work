313eb8a4aec3f846323ac79b2f23cb37f10e15d7
Juls1810 <Ladi1993@mail.ru>
2025-09-13 15:13:57 +0300

    финальные исправления

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Final-work
+# pytest_ui_api_template
+
+## Шаблон для автоматизации тестирования на python
+
+### Стек:
+- pytest
+- keyboard
+- selenium
+- requests
+- _sqlalchemy_from selenium.webdriver.common.by import By
+- sqlalchemy_from selenium.webdriver.common.by import Keys
+- sqlalchemy_from selenium.webdriver.common.by import WebDriverWait
+- sqlalchemy_from selenium.webdriver.common.by import as EC
+
+Данные тесты предназначены для тестирования API и UI
--- test_API.py
+++ test_API.py
@@ -1,19 +1,31 @@
 import requests
 import pytest
+import allure
 
 base_url = "https://api.kinopoisk.dev/"
 headers = {
         'X-API-KEY': 'VPCDTQ7-47B44DY-QBGZ77D-YM7HV9D',
         'Content-Type': 'application/json'
     }
+@allure.title("API тестирование сервиса Кинопоиск")
+@allure.description("Тестs проверяют возможность авторизации на портале с валидным и невалидным номером телефона,"
+                    " а так же поиск фильмов,жанров и актеров")
+@allure.feature("Сервис Кинопоиск")
+@allure.severity(allure.severity_level.CRITICAL)
 
 def test_kp_search_by_id():
+    """
+        Тест проверяет поиск фильма по id
+    """
     resp = requests.get(url=base_url + '/v1.4/movie/666', headers=headers)
     assert resp.status_code == 200
     assert resp.json()["id"] == 666
     assert resp.json()["name"] == "Форсаж"
 
 def test_kp_search_by_name():
+    """
+            Тест проверяет поиск фильма по названию
+        """
     resp = requests.get(url=base_url + '/v1.4/movie/search?page=1&limit=10&query=zootopia', headers=headers)
     assert resp.status_code == 200
     assert resp.json().get("docs", [{}])[0].get("id") == 775276
@@ -21,6 +33,9 @@ def test_kp_search_by_name():
 
 
 def test_kp_search_seasons():
+    """
+            Тест проверяет поиск сезона сериала
+        """
     resp = requests.get(url=base_url + '/v1.4/season?notNullFields=enName&movieId=571335&number=10', headers=headers)
     assert resp.status_code == 200
     assert resp.json().get("docs", [{}])[0].get("movieId") == 571335
@@ -28,11 +43,17 @@ def test_kp_search_seasons():
 
 
 def test_kp_search_by_id_post():
+    """
+            Тест проверяет поиск методом post
+    """
     resp = requests.post(url=base_url + '/v1.4/movie/666', headers=headers)
     assert resp.status_code == 404
 
 
 def test_kp_search_by_id_delete():
+    """
+            Тест проверяет возможность использования метода delite
+        """
     resp = requests.delete(url=base_url + '/v1.4/movie/666', headers=headers)
     assert resp.status_code == 404
 
